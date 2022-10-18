c19667fe2130a0499112a8159a83ea4785316b11
Richard Lin <35508487+richardlin047@users.noreply.github.com>
2022-10-17 17:40:24 -0700

    Update ErrorModel swagger to fix ErrorModel enum deserialization (#38)
    Updates the code generated file for ErrorModel.java. This adds a @JsonCreator annotation on the toString method, which allows the values to be correctly deserialized from the enum value instead of the name.
    
    This annotation is added onto many other enums, but they have not been added in this PR because there are multiple breaking changes. This solution replaces https://github.com/rockset/swagger-codegen/pull/2 and addresses https://github.com/rockset/rockset-java-client/issues/37

--- src/test/java/com/rockset/client/api/TestResponseMapper.java
+++ src/test/java/com/rockset/client/api/TestResponseMapper.java
@@ -1,7 +1,10 @@
 package com.rockset.client.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.rockset.client.ApiClient;
 import com.rockset.client.ApiException;
+import com.rockset.client.model.ErrorModel;
 import com.squareup.okhttp.Response;
 import com.squareup.okhttp.ResponseBody;
 import org.mockito.Mockito;
@@ -21,5 +24,26 @@ public class TestResponseMapper {
 
         ApiException e = Assert.expectThrows(ApiException.class, () -> apiClient.handleResponse(response, null));
         Assert.assertEquals(e.getErrorModel().getMessage(), "invalid");
+    }
+
+    @Test
+    public void testQueryError() throws Exception {
+        // Set up error response
+        ErrorModel error = new ErrorModel().message("Field `abc` not found.").type(ErrorModel.TypeEnum.QUERY_ERROR).line(1).column(15);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING);
+        String errorStr = mapper.writeValueAsString(error);
+
+        ResponseBody body = Mockito.mock(ResponseBody.class);
+        Mockito.when(body.string()).thenReturn(errorStr);
+
+        Response response = Mockito.mock(Response.class);
+        Mockito.when(response.isSuccessful()).thenReturn(false);
+        Mockito.when(response.body()).thenReturn(body);
+
+        // Check deserialization
+        ApiClient apiClient = new ApiClient();
+        ApiException e = Assert.expectThrows(ApiException.class, () -> apiClient.handleResponse(response, null));
+        Assert.assertEquals(e.getErrorModel(), error);
     }
 }
